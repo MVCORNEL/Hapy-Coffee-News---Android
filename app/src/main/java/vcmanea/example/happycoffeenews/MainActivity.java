0badf1f37dfdde15f151c70668d6c42dd7e432cd
ManeaValentinCornel <manea.valentin.cornel@gmail.com>
2019-05-05 17:26:21 +0100

    Implementation of NavigationDrawer: -Toolbar -Menu items -Toogle -NavigationView.OnNavigationViewClickListener(empty)

--- app/src/main/java/vcmanea/example/happycoffeenews/MainActivity.java
+++ app/src/main/java/vcmanea/example/happycoffeenews/MainActivity.java
@@ -1,13 +1,47 @@
 package vcmanea.example.happycoffeenews;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    private Toolbar mToolbar;
+    private NavigationView mNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //****************  TOOLBAR ******************//
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        //****************  TOOGLE ******************//
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.open_toggle, R.string.close_toggle);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        //****************  OnClickListener for NAVIGATION VIEW******************//
+        mNavigationView = findViewById(R.id.navigation_view);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                }
+
+                return true;
+            }
+        });
+
     }
+
+
 }
