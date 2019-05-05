82f6f57bc3649b0c56348626d421968c6575c3f5
ManeaValentinCornel <manea.valentin.cornel@gmail.com>
2019-05-05 14:19:59 +0100

    First commit!

---
+++ app/src/main/java/vcmanea/example/happycoffeenews/MainActivity.java
@@ -0,0 +1,13 @@
+package vcmanea.example.happycoffeenews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
